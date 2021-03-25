9e6b01011bd8053ce3e2580d86286a8138df9c4e
bloshound <bloshound@gmail.com>
2021-03-25 19:28:03 +0300

    first

---
+++ src/main/java/com/bloshound/filmarchive/FilmarchiveApplication.java
@@ -0,0 +1,13 @@
+package com.bloshound.filmarchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmarchiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmarchiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bloshound/filmarchive/FilmarchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloshound.filmarchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmarchiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
